cbc3d25e92974f8a3a4de2efb236a05524836805
Rodrigo Souza <rodrigaosouza27@hotmail.com>
2021-04-07 20:52:02 -0300

    Update addField.js

--- js/addField.js
+++ js/addField.js
@@ -1,1 +1,17 @@
-document.querySelector("container").style
+// procurar o botao
+// quando clicar no botao 
+document.querySelector("add-time")
+.addEventListener("click", cloneField)
+
+
+
+//  executar uma ação
+function cloneField() {
+    console.log("clooooo")
+}
+
+
+//duplicar os campos 
+
+
+//colocar na pagina
