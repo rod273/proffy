c3cf68a3a98b98a73b030a99d1762ec3eaf0c73f
Rodrigo Souza <rodrigaosouza27@hotmail.com>
2021-04-07 21:20:24 -0300

    Update addField.js

--- js/addField.js
+++ js/addField.js
@@ -9,10 +9,24 @@ document.querySelector("#add-time")
 function cloneField() {
 
  //duplicar os campos. q campos?
- fields = document.querySelector(".schedule-item").cloneNode(true)
+  const newFieldContainer = document.querySelector(".schedule-item").cloneNode(true)
+
+  //pegar os campos.. q campos?
+  const fields = newFieldContainer.querySelectorAll("input")
+
+ //para cada campo limpar
+
+ fields.forEach(function(field) {
+
+     //pegar o field do momento e limpa ele
+     field.value=""
+
+
+ })
+
 
 //colocar na pagina
-document.querySelector("#schedule-items").appendChild(fields)
+document.querySelector("#schedule-item").appendChild(newFieldContainer)
 }
 
 
